e218b9bd8ea93ad75fb55518d9b21614b605b878
hirohab <hiroshab@bluechip.lk>
2025-01-02 09:52:34 +0530

    finalize

--- src/seleniumTestProject/SearchTraining.java
+++ src/seleniumTestProject/SearchTraining.java
@@ -12,18 +12,31 @@ public class SearchTraining {
 	WebDriver driver;
 	
 	public void LaunchBrowser() {
-		System.setProperty("webdriver.gecko.driver","drivers/geckodriver.exe");
+		 System.setProperty("webdriver.gecko.driver","drivers/geckodriver");
 		driver = new FirefoxDriver();
 		driver.get("https://www.simplilearn.com");
 	}
 	
-	public void search() {
-		driver.findElement(By.id("header_srch")).sendKeys("");
+	public void search() throws InterruptedException {
+		driver.findElement(By.id("header_srch")).sendKeys("Selenium");
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//span[@class='search_icon']")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//h5[text()='Introduction to Selenium']")).click();
+		System.out.println("The page titile is: "+ driver.getTitle());
+	}
+	
+	public void closeBrowser() {
+		driver.quit(); 
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+
+		SearchTraining obj = new SearchTraining();
+		obj.LaunchBrowser();
+//		obj.search();
+//		obj.closeBrowser();
 		
-		System.out.println("seacrch by selenium works");
 	}
 
 }
--- src/seleniumTestProject/myTestClass.java
+++ src/seleniumTestProject/myTestClass.java
@@ -2,7 +2,7 @@ package seleniumTestProject;
 
 public class myTestClass {
 	public static void main(String []args) {
-		System.out.println("hello world"+"C:\\seleniumWebdriver\\geckodriver-v0.35.0-win-aarch64");
+//		System.out.println("hello world"+"C:\\seleniumWebdriver\\geckodriver-v0.35.0-win-aarch64");
 		
 	}
 }
